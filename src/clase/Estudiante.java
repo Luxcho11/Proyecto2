31fc99c9e97e24d137e41cd213fbe0fe89ad329f
Rayel Ramírez Medrano <johel8m@gmail.com>
2025-08-15 13:52:13 -0600

    Aqui se creo el proyecto, 2 paquetes y la clase Estudiante

---
+++ src/clase/Estudiante.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package clase;
+
+/**
+ *
+ * @author Huawei
+ */
+public class Estudiante {
+    //Aquí vaa a trabajar Yamil, en conjunto con todos. 
+}
