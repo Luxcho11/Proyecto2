9cb783cf255742a728a639c1623a1ee43e1c7c16
Valeria <valemorales0511@gmail.com>
2025-08-16 20:51:45 -0600

    se agregan las clases Beneficios y Carreras

---
+++ src/clase/Beneficios.java
@@ -0,0 +1,58 @@
+
+package clase;
+
+public class Beneficios {
+    
+    private int idBeneficio;
+    private String nomBeneficio;
+    private String descripcion;
+    private double montoBeneficio;
+
+    public Beneficios(int idBeneficio, String nomBeneficio, String descripcion, double montoBeneficio) {
+        this.idBeneficio = idBeneficio;
+        this.nomBeneficio = nomBeneficio;
+        this.descripcion = descripcion;
+        this.montoBeneficio = montoBeneficio;
+    }
+    
+     public Beneficios() {
+        this.idBeneficio = 0;
+        this.nomBeneficio = "";
+        this.descripcion = "";
+        this.montoBeneficio = 0;
+    }
+
+    public int getIdBeneficio() {
+        return idBeneficio;
+    }
+
+    public void setIdBeneficio(int idBeneficio) {
+        this.idBeneficio = idBeneficio;
+    }
+
+    public String getNomBeneficio() {
+        return nomBeneficio;
+    }
+
+    public void setNomBeneficio(String nomBeneficio) {
+        this.nomBeneficio = nomBeneficio;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public double isMontoBeneficio() {
+        return montoBeneficio;
+    }
+
+    public void setMontoBeneficio(double montoBeneficio) {
+        this.montoBeneficio = montoBeneficio;
+    }
+     
+     
+}
